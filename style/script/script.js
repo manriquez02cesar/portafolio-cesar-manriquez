51d3385f3c077360bfa479b3fa94e834f46148bc
manriquez02cesar <manriquez02cesar@gmail.com>
2026-04-13 17:47:22 -0600

    Actualizando mi portafolio y optimizando mi codigo

--- style/script/script.js
+++ style/script/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     video.className = "video-bg";
 
     const source = document.createElement("source");
-    source.src = "../vds/body5.mp4";
+    source.src = "../componentes/vds/body.mp4";
     source.type = "video/mp4";
 
     video.appendChild(source);
