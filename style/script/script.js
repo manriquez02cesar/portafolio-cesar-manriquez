62144b7bfbffed33a8b25be588afb91cc8c762ab
manriquez02cesar <manriquez02cesar@gmail.com>
2026-04-14 22:24:49 -0600

    Sacando el index para que el github pages lo pueda leer

--- style/script/script.js
+++ style/script/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     video.className = "video-bg";
 
     const source = document.createElement("source");
-    source.src = "../componentes/vds/body.mp4";
+    source.src = "componentes/vds/body.mp4";
     source.type = "video/mp4";
 
     video.appendChild(source);
